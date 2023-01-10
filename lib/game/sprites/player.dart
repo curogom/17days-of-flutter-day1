288bdfec795d8eb64564a3404840c873f1a64cb5
curogom <i_am@curogom.dev>
2023-01-10 23:30:59 +0900

    step4 - add a player to the game

--- lib/game/sprites/player.dart
+++ lib/game/sprites/player.dart
@@ -46,6 +46,8 @@ class Player extends SpriteGroupComponent<PlayerState>
   Future<void> onLoad() async {
     await super.onLoad();
 
+    await _loadCharacterSprites();
+    current = PlayerState.center;
     // Core gameplay: Add circle hitbox to Dash
 
     // Add a Player to the game: loadCharacterSprites
@@ -55,17 +57,27 @@ class Player extends SpriteGroupComponent<PlayerState>
   @override
   void update(double dt) {
     // Add a Player to the game: Add game state check
+    if (gameRef.gameManager.isIntro || gameRef.gameManager.isGameOver) return;
 
     // Add a Player to the game: Add calcualtion for Dash's horizontal velocity
+    _velocity.x = _hAxisInput * jumpSpeed;
 
     final double dashHorizontalCenter = size.x / 2;
 
     // Add a Player to the game: Add infinite side boundaries logic
+    if (position.x < dashHorizontalCenter) {
+      position.x = gameRef.size.x - (dashHorizontalCenter);
+    }
+
+    if (position.x > gameRef.size.x - (dashHorizontalCenter)) {
+      position.x = dashHorizontalCenter;
+    }
 
     // Core gameplay: Add gravity
 
     // Add a Player to the game: Calculate Dash's current position based on
     // her velocity over elapsed time since last update cycle
+    position += _velocity * dt;
     super.update(dt);
   }
 
@@ -74,6 +86,17 @@ class Player extends SpriteGroupComponent<PlayerState>
     _hAxisInput = 0;
 
     // Add a Player to the game: Add keypress logic
+    if (keysPressed.contains(LogicalKeyboardKey.arrowLeft)) {
+      moveLeft();
+    }
+
+    if (keysPressed.contains(LogicalKeyboardKey.arrowRight)) {
+      moveRight();
+    }
+
+    if (keysPressed.contains(LogicalKeyboardKey.arrowUp)) {
+      // jump();
+    }
 
     return true;
   }
@@ -82,12 +105,16 @@ class Player extends SpriteGroupComponent<PlayerState>
     _hAxisInput = 0;
 
     // Add a Player to the game: Add logic for moving left
+    current = PlayerState.left;
+    _hAxisInput += movingLeftInput;
   }
 
   void moveRight() {
     _hAxisInput = 0;
 
     // Add a Player to the game: Add logic for moving right
+    current = PlayerState.right;
+    _hAxisInput += movingRightInput;
   }
 
   void resetDirection() {
